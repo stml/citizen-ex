b7481376f72840e20ee155cb8ef4aebf3a0b9b30
ntlk <hi@ntlk.net>
2015-04-13 15:44:02 +0100

    Create LogEntries and store them in an array for now

---
+++ chrome/citizen-ex.js
@@ -0,0 +1,87 @@
+var LogEntry = function(url, timestamp) {
+  this.url = url;
+  this.domain = utils.trimUrl(url);
+  this.timestamp = timestamp;
+  this.getOwnGeo();
+  this.getRemoteGeo(this.domain);
+};
+
+LogEntry.prototype.getOwnGeo = function() {
+  utils.get('https://freegeoip.net/json', _.bind(function(response) {
+    var json = JSON.parse(response);
+    this.ownIp = json.ip;
+    this.ownCountryCode = json.country_code;
+    this.ownRegionCode = json.region_code;
+    this.ownCity = json.city;
+    this.ownLat = json.latitude;
+    this.ownLng = json.longitude;
+  }, this));
+};
+
+LogEntry.prototype.getRemoteGeo = function(url) {
+  utils.get('https://freegeoip.net/json/' + url, _.bind(function(response) {
+    var json = JSON.parse(response);
+    this.ip = json.ip;
+    this.countryCode = json.country_code;
+    this.regionCode = json.region_code;
+    this.city = json.city;
+    this.lat = json.latitude;
+    this.lng = json.longitude;
+  }, this));
+
+}
+
+var Utils = function() {};
+
+Utils.prototype.createLogEntry = function(tab) {
+  var timestamp = new Date();
+  logEntries.push(new LogEntry(tab.url, timestamp));
+}
+
+Utils.prototype.trimUrl = function(url) {
+  var uri = new URI(url);
+  var path = uri.domain();
+  return path;
+}
+
+Utils.prototype.log = function(thing) {
+  console.log(thing);
+};
+
+Utils.prototype.get = function(url, callback) {
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', url, true);
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState == 4) {
+      callback(xhr.responseText);
+    }
+  }
+  xhr.send();
+};
+
+var utils = new Utils();
+
+var ChromeUtils = function() {};
+
+ChromeUtils.prototype.getTabById = function(tabId, callback) {
+  // we use a callback here because chrome.tabs.get is asynchronous
+  chrome.tabs.get(tabId, callback);
+};
+
+var chromeUtils = new ChromeUtils();
+
+var logEntries = [];
+
+chrome.tabs.onUpdated.addListener(function(tabId) {
+  chromeUtils.getTabById(tabId, utils.createLogEntry);
+});
+
+chrome.tabs.onCreated.addListener(function(tab) {
+  utils.createLogEntry(tab);
+});
+
+// fires when an existing tab is selected
+chrome.tabs.onActivated.addListener(function(activeInfo) {
+  var tabId = activeInfo.tabId;
+  chromeUtils.getTabById(tabId, utils.createLogEntry);
+});
