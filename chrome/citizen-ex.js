db4a369687069d8c0f64f7dc1e4c46d918353ae5
ntlk <hi@ntlk.net>
2015-04-13 17:30:16 +0100

    Update the records in local storage ever time we have some new data

--- chrome/citizen-ex.js
+++ chrome/citizen-ex.js
@@ -15,6 +15,7 @@ LogEntry.prototype.getOwnGeo = function() {
     this.ownCity = json.city;
     this.ownLat = json.latitude;
     this.ownLng = json.longitude;
+    chrome.storage.local.set({ 'logEntries': logEntries });
   }, this));
 };
 
@@ -27,6 +28,7 @@ LogEntry.prototype.getRemoteGeo = function(url) {
     this.city = json.city;
     this.lat = json.latitude;
     this.lng = json.longitude;
+    chrome.storage.local.set({ 'logEntries': logEntries });
   }, this));
 
 }
@@ -42,7 +44,6 @@ Utils.prototype.createLogEntry = function(tab) {
 
   var timestamp = new Date();
   logEntries.push(new LogEntry(tab.url, timestamp));
-  chrome.storage.local.set({ 'logEntries': logEntries });
 }
 
 Utils.prototype.trimUrl = function(url) {
