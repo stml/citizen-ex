092f9c1b04490f06dc8b76143ec12e07598c9623
ntlk <hi@ntlk.net>
2015-04-13 17:26:59 +0100

    Show the LogEntry object as a JSON string on the popup page

---
+++ chrome/popup.js
@@ -0,0 +1,6 @@
+chrome.storage.local.get('logEntries', function(entries) {
+  var logEntries = entries.logEntries;
+  var lastEntry = logEntries[logEntries.length - 1];
+  var logString = JSON.stringify(lastEntry);
+  document.getElementById('content').textContent = logString;
+});
