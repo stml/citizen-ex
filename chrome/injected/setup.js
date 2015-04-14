813dd1320c10300f08335485e00cd65e3adf0824
ntlk <hi@ntlk.net>
2015-04-14 14:29:23 +0100

    Load pane template from a string

--- chrome/injected/setup.js
+++ chrome/injected/setup.js
@@ -1,11 +1,16 @@
-var Pane = function(name) {
+var currentTab = '<h1>This page</h1>';
+var about = '<h1>About</h1>';
+var settings = '<h1>Settings</h1>';
+
+var Pane = function(name, template) {
   this.name = name;
+  this.template = template;
 };
 
 var panes = [
-  new Pane('currentTab'),
-  new Pane('about'),
-  new Pane('settings')
+  new Pane('currentTab', currentTab),
+  new Pane('about', about),
+  new Pane('settings', settings)
 ];
 
 var Sidebar = Backbone.Model.extend({
@@ -22,10 +27,12 @@ var SidebarPane = Backbone.View.extend({
 
   initialize: function(options) {
     this.name = options.name;
+    this.template = _.template(options.template);
     this.listenTo(this.model, 'change:activePane', function(model, pane) {
       this.render(model, pane);
     })
     this.appendToBody();
+    this.loadPaneContents();
     this.render(this.model, this.model.get('activePane'));
   },
 
@@ -33,21 +40,31 @@ var SidebarPane = Backbone.View.extend({
     switch (pane.name) {
       case this.name:
         this.$el.show();
+        this.$el.html(this.template(this.model.attributes));
+        return this;
         break;
       default:
         this.$el.hide();
         break;
     }
+
   },
 
   appendToBody: function() {
     var body = $('body');
     this.$el.appendTo(body);
+  },
+
+  loadPaneContents: function() {
+    var pane = _.find(this.model.panes, _.bind(function(pane) {
+      return pane.name === this.name;
+    }, this));
+    console.log(pane);
   }
 });
 
 var sidebar = new Sidebar(panes);
 
 _.each(panes, function(pane) {
-  new SidebarPane({ name: pane.name, model: sidebar });
+  new SidebarPane({ name: pane.name, model: sidebar, template: pane.template });
 });
