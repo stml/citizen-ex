267584a15d448febeb3478e327bd21db1f7e7fbe
ntlk <hi@ntlk.net>
2015-04-14 13:43:21 +0100

    Remove the setup stuff from the browserAction file

--- chrome/browserAction.js
+++ chrome/browserAction.js
@@ -1,34 +1,6 @@
-var Sidebar = Backbone.Model.extend({
-
-});
-
-var SidebarPane = Backbone.View.extend({
-  initialize: function(name) {
-    this.name = name;
-    this.listenTo(this.model, 'change:activePane', function(model, pane) {
-      this.render(model, pane);
-    })
-  },
-
-  render: function(model, pane) {
-    switch (pane.name) {
-      case this.name:
-        this.$el.show();
-        break;
-      default:
-        this.$el.hide();
-        break;
-    }
-  }
-});
-
-
 chrome.storage.local.get('logEntries', function(entries) {
   var logEntries = entries.logEntries;
   var lastEntry = logEntries[logEntries.length - 1];
   var logString = JSON.stringify(lastEntry);
   console.log(logString);
 });
-
-
-
