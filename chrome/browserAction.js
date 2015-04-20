816d9d37a8c17792cd79784f5e1075900d217d70
ntlk <hi@ntlk.net>
2015-04-20 12:06:25 +0100

    Remove unused files

--- chrome/browserAction.js
+++
@@ -1,6 +0,0 @@
-chrome.storage.local.get('logEntries', function(entries) {
-  var logEntries = entries.logEntries;
-  var lastEntry = logEntries[logEntries.length - 1];
-  var logString = JSON.stringify(lastEntry);
-  console.log(logString);
-});
