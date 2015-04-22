c0550357b8ee704f00c38088e1dd0360c9dfe66b
ntlk <hi@ntlk.net>
2015-04-22 17:23:31 +0100

    Render out own own scripts

--- extensions/chrome/injected/setup.js
+++ extensions/chrome/injected/setup.js
@@ -297,3 +297,8 @@ _.each(panes, function(pane) {
 });
 
 
+
+function cex_load_map() {
+	L.mapbox.accessToken = 'pk.eyJ1Ijoic3RtbCIsImEiOiJDQ1FDcFNVIn0.C7ThVrFnQ7a7COlJe8tARw';
+	L.mapbox.map('cex_map', 'stml.l6086pbg');
+}
