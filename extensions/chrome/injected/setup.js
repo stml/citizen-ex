e02fcc075a57f5366c5d913828abcc359b1ae22a
stml <james@shorttermmemoryloss.com>
2015-04-22 17:20:13 +0100

    Dunno, sorry

--- extensions/chrome/injected/setup.js
+++ extensions/chrome/injected/setup.js
@@ -297,8 +297,3 @@ _.each(panes, function(pane) {
 });
 
 
-
-function cex_load_map() {
-	L.mapbox.accessToken = 'pk.eyJ1Ijoic3RtbCIsImEiOiJDQ1FDcFNVIn0.C7ThVrFnQ7a7COlJe8tARw';
-	L.mapbox.map('cex_map', 'stml.l6086pbg');
-}
