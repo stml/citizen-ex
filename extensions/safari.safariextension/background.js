53c041530eef4665932cdbe3bdbaca3bd02e1c4d
ntlk <hi@ntlk.net>
2015-04-24 12:15:06 +0100

    Add the missing Browser class

--- extensions/safari.safariextension/background.js
+++ extensions/safari.safariextension/background.js
@@ -7,6 +7,24 @@
  *
  */
 
+var Browser = function() {
+  this.name = 'unknown';
+  var notChrome = _.isUndefined(window.chrome);
+  if (!notChrome) {
+    this.name = 'chrome';
+  } else {
+    this.name = 'safari';
+  }
+};
+
+Browser.prototype.chrome = function() {
+  return this.name === 'chrome';
+};
+
+Browser.prototype.safari = function() {
+  return this.name === 'safari';
+};
+
 var Storage = function(browser) {
   this.browser = browser;
 };
