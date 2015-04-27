44e85ebccf5bee8a74b19e72ac4d42374c02f0eb
ntlk <hi@ntlk.net>
2015-04-27 11:34:49 +0100

    Pass tab url instead of the tab object in Safari init

--- extensions/core/js/safari/init.js
+++ extensions/core/js/safari/init.js
@@ -1,9 +1,9 @@
 safari.application.addEventListener('navigate', function(stuff) {
-  console.log(safari.application.activeBrowserWindow.activeTab);
-  utils.createLogEntry(safari.application.activeBrowserWindow.activeTab);
+  // make a new entry
+  utils.createLogEntry(safari.application.activeBrowserWindow.activeTab.url);
 }, true);
 
-safari.application.addEventListener('navigate', function(stuff) {
-  console.log(safari.application.activeBrowserWindow.activeTab);
-  utils.createLogEntry(safari.application.activeBrowserWindow.activeTab);
+safari.application.addEventListener('activate', function(stuff) {
+  // update timestamp
+  utils.updateLogEntry(safari.application.activeBrowserWindow.activeTab.url);
 }, true);
