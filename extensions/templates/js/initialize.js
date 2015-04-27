941b56c2fa1ec073c29ad0fd16f13883afcccb69
ntlk <hi@ntlk.net>
2015-04-27 11:09:59 +0100

    Initialize every obejct with the browser if necessary

--- extensions/templates/js/initialize.js
+++ extensions/templates/js/initialize.js
@@ -2,6 +2,10 @@ var currentTab = INCLUDE_FILE('../html/index.html');
 var about = INCLUDE_FILE('../html/about.html');
 var settings = INCLUDE_FILE('../html/settings.html');
 
+var browser = new Browser();
+var storage = new CxStorage(browser);
+var message = new CxMessage(browser);
+
 var Pane = function(name, template) {
   this.name = name;
   this.template = template;
@@ -13,7 +17,7 @@ var panes = [
   new Pane('settings', settings)
 ];
 
-var sidebar = new Sidebar(panes);
+var sidebar = new Sidebar(panes, browser);
 
 _.each(panes, function(pane) {
   new SidebarPane({ name: pane.name, model: sidebar, template: pane.template });
