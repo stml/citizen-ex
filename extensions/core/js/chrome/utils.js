5184136b77669a12cdd857c27b374da63e99860a
ntlk <hi@ntlk.net>
2015-04-27 11:17:58 +0100

    Fetch tab only by url, not tabId and windowId

--- extensions/core/js/chrome/utils.js
+++ extensions/core/js/chrome/utils.js
@@ -4,12 +4,8 @@ var ChromeUtils = function() {};
 
 ChromeUtils.prototype.getTabById = function(tabId, callback) {
   // we use a callback here because chrome.tabs.get is asynchronous
-  chrome.tabs.get(tabId, callback);
-};
-
-ChromeUtils.prototype.findEntryForTab = function(tab) {
-  return _.find(logEntries, function(entry) {
-    return (tab.url === entry.url && tab.id === entry.tabId && tab.windowId === entry.windowId);
+  chrome.tabs.get(tabId, function(tab){
+    callback(tab.url);
   });
 };
 
