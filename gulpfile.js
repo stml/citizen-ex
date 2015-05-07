6f71420b47cc0cba6117b63663515525f1072ded
stml <james@shorttermmemoryloss.com>
2015-05-07 14:09:47 +0100

    Fixed Heatmap from Mapbox switch

--- gulpfile.js
+++ gulpfile.js
@@ -20,7 +20,8 @@ var pageSources = [
   './extensions/shared/js/cx_extension.js',
   './extensions/page/js/cx_page.js',
   './extensions/page/js/cx_page_view.js',
-  './extensions/page/js/init_shared.js'
+  './extensions/page/js/init_shared.js',
+  './extensions/page/js/leaflet-heat.js'
 ];
 
 var panelCssSources = [
