587620c25719cd3247778242d63758f5ac38ae09
stml <james@shorttermmemoryloss.com>
2015-05-08 09:13:09 +0100

    Improved Share Badge

--- extensions/chrome/panel/panel.js
+++ extensions/chrome/panel/panel.js
@@ -1416,39 +1416,21 @@ var message = new CxMessage(browser);
 var cxPanel;
 var cxPanelView;
 
-// panel/js/init_safari.js
+// panel/js/init_chrome.js
 
-var cxPanel = false;
-
-safari.self.addEventListener('message', function(message) {
-
-  if (message.name === 'tabs') {
-    cxPanel.receiveOpenTabs(message.message.tabs);
-  } else if (message.name === 'activeTab') {
-    cxPanel.receiveActiveTab(message.message.activeTab);
-  } else if (message.name === 'allLogEntries') {
-    cxPanel.receiveAllLogEntries(message.message.allLogEntries);
-  } else if (message.name === 'countryLog') {
-    cxPanel.receiveCitizenship(message.message.countryLog);
-  } else if (message.name === 'ownGeoData') {
-    cxPanel.receiveOwnGeoData(message.message.ownGeoData);
+chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
+  if (_.has(request, 'tabs')) {
+    cxPanel.receiveOpenTabs(request.tabs);
+  } else if (_.has(request, 'activeTab'))  {
+    cxPanel.receiveActiveTab(request.activeTab);
+  } else if (request.allLogEntries) {
+    cxPanel.receiveAllLogEntries(request.allLogEntries);
+  } else if (request.countryLog) {
+    cxPanel.receiveCitizenship(request.countryLog);
+  } else if (_.has(request, 'ownGeoData')) {
+    cxPanel.receiveOwnGeoData(request.ownGeoData);
   }
+});
 
-  if (cxPanel) {
-    return;
-  }
-
-  cxPanel = new CxPanel(browser);
-  cxPanelView = new CxPanelView({ model: cxPanel, template: panelTemplate });
-
-  // we fetch the last record again
-  // as some of its request should have come back by now
-  cxPanel.requestActiveTab();
-
-  // we toggle the main cxPane pane visibility
-  if (message.name === 'openCxPanel') {
-    cxPanel.toggle();
-    cxPanel.requestOpenTabs();
-  }
-
-}, false);
+cxPanel = new CxPanel(browser);
+cxPanelView = new CxPanelView({ model: cxPanel, template: panelTemplate });
