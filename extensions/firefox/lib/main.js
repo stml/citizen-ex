19c1b95def8937f3b6ee1e4048bdb7fa4257e536
ntlk <hi@ntlk.net>
2015-05-21 21:57:51 +0100

    In Firefox change and disable / enable the icon for the relevant tab

--- extensions/firefox/lib/main.js
+++ extensions/firefox/lib/main.js
@@ -282,18 +282,31 @@ var CxIcon = function(browser, blank, local, remote, full) {
   this.full = full;
 };
 
-CxIcon.prototype.setIcon = function(iconType) {
+CxIcon.prototype.setIcon = function(iconType, tab) {
   if (this.browser.chrome()) {
     chrome.browserAction.setIcon({ path: this[iconType] });
   } else if (this.browser.safari()) {
     var iconUri = safari.extension.baseURI + this[iconType];
     safari.extension.toolbarItems[0].image = iconUri;
   } else if (this.browser.firefox()) {
-    button.icon = {
-      '16': self.data.url(this[iconType])
+    // should specify the tab
+    var iconUrl = self.data.url(this[iconType]);
+    var tab = 'tab';
+    if (typeof this.tabData === 'object') {
+      tab = this.tabData;
     }
+    button.state(tab, {
+      disabled: false,
+      icon: {
+        '16': iconUrl
+      }
+    });
   }
 };
+
+CxIcon.prototype.addTabData = function(tab) {
+  this.tabData = tab;
+}
 
 // Define and set up utilities
 
